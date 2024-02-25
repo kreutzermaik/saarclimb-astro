19f047a7aa0900dce97227bef8cdad7b570131ad
Maik Kreutzer <s17ce7@umwelt-campus.de>
2024-02-25 20:12:59 +0100

    supabaseUrl fix

--- src/supabase.ts
+++ src/supabase.ts
@@ -7,8 +7,8 @@ if (process.env.NODE_ENV === "development") {
     supabaseUrl = "https://tqgufceactcswwkbjfjk.supabase.co";
     supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZ3VmY2VhY3Rjc3d3a2JqZmprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA2OTMyOTksImV4cCI6MTk4NjI2OTI5OX0.MRYORIJPYAwuRfISOKKsKqCnfABJqLaXT1cZQZ6ZAE4";
 } else {
-    supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-    supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+    supabaseUrl = "https://ybeongwjjfdkgizzkmsc.supabase.co";
+    supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InliZW9uZ3dqamZka2dpenprbXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI4MjMzMzQsImV4cCI6MTk4ODM5OTMzNH0.NaSZptwqEuuAfhKmoCV_lHraA8nROlQmwKCxEHc8z34";
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
