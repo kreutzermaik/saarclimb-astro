fa4d6d42aa97ac7d01b744eeae38092a80b3691a
Maik Kreutzer <s17ce7@umwelt-campus.de>
2024-03-07 16:44:33 +0100

    added vitePWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
 import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import { VitePWA } from "vite-plugin-pwa"
+import { manifest } from "./seoConfig.js"
 
-// https://astro.build/config
 export default defineConfig({
     integrations: [
         tailwind(),
-        svelte()
-    ]
+        svelte(),
+    ],
+    vite: {
+        plugins: [
+            VitePWA({
+                registerType: "autoUpdate",
+                manifest,
+                workbox: {
+                    globDirectory: 'dist',
+                    globPatterns: [
+                        '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+                    ],
+                    // Don't fallback on document based (e.g. `/some-page`) requests
+                    // This removes an errant console.log message from showing up.
+                    navigateFallback: null,
+                },
+            })
+        ]
+    }
 });
